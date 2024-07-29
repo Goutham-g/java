a69df2ffaf658a6208d2359cfb3799fdbc5ce389
goutham <gouthamappu56@gmail.com>
2024-07-30 00:13:46 +0530

    uncommenting

--- src/javaBasics/Basics.java
+++ src/javaBasics/Basics.java
@@ -13,18 +13,18 @@ public class Basics {
 
     public static void main(String[] args) {
 
-        System.out.println("enter two no");
-        Scanner scan =new Scanner(System.in);
-        try{
-        int n1=scan.nextInt();
-        int n2 =scan.nextInt();
-
-            int div=n1/n2;
-            System.out.println(div);
-}
-        catch(Exception e){
-            System.out.println("the input type mismatch"+e);
-        }
+//        System.out.println("enter two no");
+//        Scanner scan =new Scanner(System.in);
+//        try{
+//        int n1=scan.nextInt();
+//        int n2 =scan.nextInt();
+//
+//            int div=n1/n2;
+//            System.out.println(div);
+//}
+//        catch(Exception e){
+//            System.out.println("the input type mismatch"+e);
+//        }
 
 
 
@@ -41,64 +41,67 @@ public class Basics {
         temp=a;
         a=b;
         b=temp;
-        System.out.println("a="+a+"and"+"b="+b);
+        System.out.println("Number swap:"+"a="+a+"and"+"b="+b);
 
-        System.out.println("enter a binary no");
+        //sum of digits
+
+        System.out.println("enter a  no");
        Scanner scan1 =new Scanner(System.in);
-       int num1=scan.nextInt();
+       int num1=scan1.nextInt();
        int sum=0;
        while(num1!=0){
            int n=num1%10;
            sum=sum+n;
            num1=num1/10;
        }
-        System.out.println(sum);
+        System.out.println("sum of digit:"+sum);
 
         //reverse the digit of an integer
 
            System.out.println("enter a  no");
           Scanner scan2 =new Scanner(System.in);
-          int numm1=scan.nextInt();
-          int i=0;
+          int numm1=scan2.nextInt();
+
           int var=0;
-         while(num1!=0) {
-             int n = num1%10;
+         while(numm1!=0) {
+             int n = numm1%10;
              var=var*10+n;
-             num1=num1/10;
+             numm1=numm1/10;
 
 
          }
-        System.out.println(var);
+        System.out.println("reverse of digit:"+var);
 
 //        Count the no of digit in an integer
 
         System.out.println("enter a digit");
         Scanner scan3 =new Scanner(System.in);
-        int num=scan.nextInt();
+        int num=scan3.nextInt();
         int count=0;
           while(num!=0){
               int n=num%10;
               num = num/10;
               count++;
           }
-        System.out.println(count);
+        System.out.println("Count:"+count);
 
           // find the largest digit in an integer
         //12345
+        int dig=56473;
              int current=0;
-        while(num!=0){
-            int n=num%10;
+        while(dig!=0){
+            int n=dig%10;
 
             if(n>current){
                 current=n;
 
             }else{
-                num=num/10;
+                dig=dig/10;
             }
 
 
         }
-        System.out.println("largest"+current);
+        System.out.println("largest digit"+current);
 
 //paliondrome
 
@@ -117,7 +120,7 @@ public class Basics {
 
 
         if(rev==ogNum){
-            System.out.println("the num is paliondrome:"+rev);
+            System.out.println("the num is paliondrome:");
         }else{
             System.out.println("not a paliondrome");
         }
@@ -133,7 +136,7 @@ public class Basics {
 
 
         }
-        System.out.println(pro+"pro");
+        System.out.println("Product of digit:"+pro);
 
 
 
